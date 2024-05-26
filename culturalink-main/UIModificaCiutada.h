49d4e65266521fdff41595ad926095552b15a71f
xvimo <xavier.vila.monge@estudiantat.upc.edu>
2024-05-26 20:37:34 +0200

    task #29: modifica ciutada Ui update

--- culturalink-main/UIModificaCiutada.h
+++ culturalink-main/UIModificaCiutada.h
@@ -78,9 +78,11 @@ namespace culturalink_main {
 
 
 	private: System::Windows::Forms::Panel^ pnlCtrMODCIT;
+	private: System::Windows::Forms::Label^ lblNomCitMODCIT;
 
-	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::Button^ button1;
+
+	private: System::Windows::Forms::Button^ btnBackMODCIT;
+
 
 	private:
 		/// <summary>
@@ -96,13 +98,13 @@ namespace culturalink_main {
 		void InitializeComponent(void)
 		{
 			this->PANELModificaCiut = (gcnew System::Windows::Forms::Panel());
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->btnBackMODCIT = (gcnew System::Windows::Forms::Button());
 			this->pnlRepCtrMODCIT = (gcnew System::Windows::Forms::Panel());
 			this->lblRepCtrMODCIT = (gcnew System::Windows::Forms::Label());
 			this->pnlCtrMODCIT = (gcnew System::Windows::Forms::Panel());
 			this->lblCtrMODCIT = (gcnew System::Windows::Forms::Label());
 			this->pnlTitlesMODCIT = (gcnew System::Windows::Forms::Panel());
-			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->lblNomCitMODCIT = (gcnew System::Windows::Forms::Label());
 			this->lblFormatMODCIT = (gcnew System::Windows::Forms::Label());
 			this->btnModMODCIT = (gcnew System::Windows::Forms::Button());
 			this->TextBoxRepCtrMODCIT = (gcnew System::Windows::Forms::TextBox());
@@ -128,7 +130,7 @@ namespace culturalink_main {
 			// PANELModificaCiut
 			// 
 			this->PANELModificaCiut->BackColor = System::Drawing::Color::White;
-			this->PANELModificaCiut->Controls->Add(this->button1);
+			this->PANELModificaCiut->Controls->Add(this->btnBackMODCIT);
 			this->PANELModificaCiut->Controls->Add(this->pnlRepCtrMODCIT);
 			this->PANELModificaCiut->Controls->Add(this->pnlCtrMODCIT);
 			this->PANELModificaCiut->Controls->Add(this->pnlTitlesMODCIT);
@@ -148,16 +150,16 @@ namespace culturalink_main {
 			this->PANELModificaCiut->TabIndex = 15;
 			this->PANELModificaCiut->Visible = false;
 			// 
-			// button1
+			// btnBackMODCIT
 			// 
-			this->button1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Right));
-			this->button1->Font = (gcnew System::Drawing::Font(L"Yu Gothic UI", 7.8F, System::Drawing::FontStyle::Bold));
-			this->button1->Location = System::Drawing::Point(347, 473);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(76, 27);
-			this->button1->TabIndex = 38;
-			this->button1->Text = L"Back";
-			this->button1->UseVisualStyleBackColor = true;
+			this->btnBackMODCIT->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Right));
+			this->btnBackMODCIT->Font = (gcnew System::Drawing::Font(L"Yu Gothic UI", 7.8F, System::Drawing::FontStyle::Bold));
+			this->btnBackMODCIT->Location = System::Drawing::Point(347, 473);
+			this->btnBackMODCIT->Name = L"btnBackMODCIT";
+			this->btnBackMODCIT->Size = System::Drawing::Size(76, 27);
+			this->btnBackMODCIT->TabIndex = 38;
+			this->btnBackMODCIT->Text = L"Back";
+			this->btnBackMODCIT->UseVisualStyleBackColor = true;
 			// 
 			// pnlRepCtrMODCIT
 			// 
@@ -203,22 +205,22 @@ namespace culturalink_main {
 			// pnlTitlesMODCIT
 			// 
 			this->pnlTitlesMODCIT->BackColor = System::Drawing::Color::Black;
-			this->pnlTitlesMODCIT->Controls->Add(this->label1);
+			this->pnlTitlesMODCIT->Controls->Add(this->lblNomCitMODCIT);
 			this->pnlTitlesMODCIT->Dock = System::Windows::Forms::DockStyle::Top;
 			this->pnlTitlesMODCIT->Location = System::Drawing::Point(0, 0);
 			this->pnlTitlesMODCIT->Name = L"pnlTitlesMODCIT";
 			this->pnlTitlesMODCIT->Size = System::Drawing::Size(586, 38);
 			this->pnlTitlesMODCIT->TabIndex = 32;
 			// 
-			// label1
+			// lblNomCitMODCIT
 			// 
-			this->label1->AutoSize = true;
-			this->label1->ForeColor = System::Drawing::Color::White;
-			this->label1->Location = System::Drawing::Point(33, 12);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(82, 13);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"NOM CIUTADA";
+			this->lblNomCitMODCIT->AutoSize = true;
+			this->lblNomCitMODCIT->ForeColor = System::Drawing::Color::White;
+			this->lblNomCitMODCIT->Location = System::Drawing::Point(33, 12);
+			this->lblNomCitMODCIT->Name = L"lblNomCitMODCIT";
+			this->lblNomCitMODCIT->Size = System::Drawing::Size(82, 13);
+			this->lblNomCitMODCIT->TabIndex = 0;
+			this->lblNomCitMODCIT->Text = L"NOM CIUTADA";
 			// 
 			// lblFormatMODCIT
 			// 
